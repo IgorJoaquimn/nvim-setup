bd1eb4088ec256d29197981108cb5ac33826164b
IgorJoaquimn <igor-joaquim@ufmg.br>
2024-10-15 18:18:22 -0300

    Removed packer from .local/share and changed the treesitter settings ( come back to ensure installed )

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,6 +8,7 @@ local M = {
         local configs = require('nvim-treesitter.configs')
 
         configs.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline","javascript", "python" },
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
 
