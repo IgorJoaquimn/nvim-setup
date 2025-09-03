ea0b9576e5ea380bf52eca27e5420300715959e3
IgorJoaquimn <igor-joaquim@ufmg.br>
2025-09-03 20:28:32 -0300

    fix dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -49,7 +49,7 @@ return {
                 type = "server",
                 port = "${port}",
                 executable = {
-                    command = "/home/igorsc/.local/share/nvim/mason/bin/codelldb",
+                    command = os.getenv("HOME") .. "/.local/share/nvim/mason/bin/codelldb",
                     args = { "--port", "${port}" },
                 },
             }
