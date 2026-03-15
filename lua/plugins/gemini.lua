2e697212452f90aaa8cc29d3c36c6ca486837aa5
igor-quino <igor.quino@example.com>
2026-03-15 19:43:11 -0300

    Correct Gemini plugin command for lazy-loading

--- lua/plugins/gemini.lua
+++ lua/plugins/gemini.lua
@@ -4,11 +4,12 @@ return {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
     },
+    cmd = { "Gemini" },
     opts = {
         -- You can add your configuration here
         -- See the plugin's documentation for available options
     },
     keys = {
-        { "<leader>gc", "<cmd>GeminiChat<cr>", desc = "Gemini Chat" },
+        { "<leader>gc", "<cmd>Gemini<cr>", desc = "Gemini Chat" },
     },
 }
